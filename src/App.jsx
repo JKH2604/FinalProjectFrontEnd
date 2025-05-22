ec095d6e2f2bc22e3b5245c60cb5a5cefb4f6eca
trung2004official <trung2004tech@gmail.com>
2025-05-22 16:23:42 +0700

    set-up-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api')
+      .then(res => setMessage(res.data.message))
+      .catch(err => console.error('Error: ',err));
+  },[]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Trung day</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='bg-red-500'>
+      <h1 class="text-3xl font-bold underline">
+        {message}
+      </h1>
+    </div>
     </>
   )
 }
